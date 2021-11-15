6f86fb168bb32894291771fceac3f58c96cbad04
Gregorio Santos <gregoriohd@gmail.com>
2021-11-15 13:17:48 -0300

    DescontoProdutos com implementação parcial

--- src/br/com/gregoriosantos/descontos/DescontosProdutos.java
+++ src/br/com/gregoriosantos/descontos/DescontosProdutos.java
@@ -1,5 +1,19 @@
 package br.com.gregoriosantos.descontos;
 
+import br.com.gregoriosantos.produtos.Produto;
+
 public class DescontosProdutos {
+	
+	private int quantidadeProdutos;
+	private Produto produtos;
+	
+	public DescontosProdutos(Produto produto, int quantidadeProdutos) {
+		
+		this.produtos = produto;
+		this.quantidadeProdutos =quantidadeProdutos;
+		
+	}
+	
+	
 
 }
