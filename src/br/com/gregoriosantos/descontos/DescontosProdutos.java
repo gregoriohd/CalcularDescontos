ef17c5653daf6efede8ec2d5e19acc15fa2f2f7a
Gregorio Santos <gregoriohd@gmail.com>
2021-11-15 22:27:35 -0300

    DescontoProdutos com implementação completa

--- src/br/com/gregoriosantos/descontos/DescontosProdutos.java
+++ src/br/com/gregoriosantos/descontos/DescontosProdutos.java
@@ -3,17 +3,34 @@ package br.com.gregoriosantos.descontos;
 import br.com.gregoriosantos.produtos.Produto;
 
 public class DescontosProdutos {
-	
+
 	private int quantidadeProdutos;
 	private Produto produtos;
-	
+
 	public DescontosProdutos(Produto produto, int quantidadeProdutos) {
-		
+
 		this.produtos = produto;
-		this.quantidadeProdutos =quantidadeProdutos;
-		
+		this.quantidadeProdutos = quantidadeProdutos;
+
 	}
-	
-	
+
+	public int getQuantidadeProdutos() {
+		return quantidadeProdutos;
+	}
+
+	public Produto getProdutos() {
+		return produtos;
+	}
+
+	public double valorDescontos() {
+
+		if (getQuantidadeProdutos() > 10 && getQuantidadeProdutos() < 21)
+			return getProdutos().getValorProduto() * 0.1;
+		if (getQuantidadeProdutos() > 20 && getQuantidadeProdutos() < 51)
+			return getProdutos().getValorProduto() * 0.2;
+		if (getQuantidadeProdutos() > 51)
+			return getProdutos().getValorProduto() * 0.25;
+		return 0;
+	}
 
 }
